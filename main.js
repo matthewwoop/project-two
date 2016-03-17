80e044e35f4e48fa0c2796a439710d738a0f7c6c
Matt Woop <matthew.woop@gmail.com>
2016-03-17 11:58:06 -0400

    prelunch work

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 window.onload = function () {
   console.log('main.js has loaded...');
-  $.ajax({
-    url: wikimedia.baseEndpoint,
-    dataType: 'jsonp',
-    success: function(response) {
-      console.log('success');
-      console.log(response);
+
+  $('#submit-btn').on('click', function() {
+    if (validInput($('#user-choice').val())){
+      $.ajax({
+        url: wiki.baseStart + $('#user-choice').val() + wiki.baseEnd,
+        dataType: 'jsonp',
+        success: function(response) {
+          console.log('success');
+          wiki.result = response.query.pages[Object.keys(response.query.pages)[0]];
+          wiki.text = wiki.result.revisions[0]['*'];
+        }
+      });
+    } else {
+      alert('Please provide an input');
+      return;
     }
   });
 }
@@ -13,7 +22,22 @@ window.onload = function () {
 
 
 
-var wikimedia = {
+var wiki = {
   baseStart: 'https://en.wikipedia.org/w/api.php?action=query&titles=',
-  baseEnd: '&prop=revisions&rvprop=content&format=json'
+  baseEnd: '&prop=revisions&rvprop=content&rvsection=0&format=json',
+  result: undefined
+}
+
+
+
+function validInput(input) {
+  if (input) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getNodes(text) {
+
 }
